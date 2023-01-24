e982ef3897fb577c6df04d0ef5d4b6e1f98b8bfe
e <lishifenging@outlook.com>
2023-01-24 18:26:39 +0800

    style: change the code style

--- project/raspberrypi4b/src/main.c
+++ project/raspberrypi4b/src/main.c
@@ -101,9 +101,9 @@ static void a_l3gd20h_interrupt_receive_callback(uint8_t type)
  * @param[in] argc is arg numbers
  * @param[in] **argv is the arg address
  * @return    status code
- *             - 0 success
- *             - 1 run failed
- *             - 5 param is invalid
+ *            - 0 success
+ *            - 1 run failed
+ *            - 5 param is invalid
  * @note      none
  */
 uint8_t l3gd20h(uint8_t argc, char **argv)
